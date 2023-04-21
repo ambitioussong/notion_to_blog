e3780b74d5daa1b409ce3756c1ca817fb5d24979
song <343949505@qq.com>
2023-04-21 14:25:14 +0800

    Update config

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Jason',
   email: 'chenjs18@163.com',
   link: 'https://blog.keepfancy.com',
-  description: 'Jason\'s daily blog',
+  description: '记录日常学习和思考',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -20,7 +20,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: '',
   seo: {
-    keywords: ['Blog', 'ChatGPT', 'Notion'],
+    keywords: ['Blog', 'ChatGPT', 'Notion', 'Daily', 'Thinkings'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
