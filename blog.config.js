f5a0c0c4ed2c4ce22c116b1df9d2da86d0e82058
song <343949505@qq.com>
2023-04-21 16:51:40 +0800

    Update config

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Jason',
   email: 'chenjs18@163.com',
   link: 'https://blog.keepfancy.com',
-  description: '记录日常学习和思考',
+  description: '广泛认知，独立思考',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
